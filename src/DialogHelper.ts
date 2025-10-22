379c39fde6fa24108813afe92038afcfc65999d0
elkuku <der.el.kuku@gmail.com>
2025-10-21 19:27:29 -0500

    Use Maps to display select options in template

--- src/DialogHelper.ts
+++ src/DialogHelper.ts
@@ -3,21 +3,40 @@ import html from './template/dialog.hbs' with {type: 'text'}
 
 import * as Handlebars from 'handlebars'
 
+Handlebars.registerHelper('eachInMap', (map: Map<string, string>, block) => {
+    let out = ''
+    for (const [key, value] of map) {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+        const str: string = block.fn({ key, value })
+        out += str
+    }
+    return out
+})
+
 export class DialogHelper {
     public getDialog(): JQuery {
         const template: HandlebarsTemplateDelegate = Handlebars.compile(html)
 
+        const selectOptions = new Map([
+            ['view', 'View'],
+            ['polygon', 'Polygon(s)'],
+        ])
+
+        const formatOptions = new Map([
+            ['json', 'JSON'],
+        ])
+
         const data = {
             plugin: 'window.plugin.ExportPortals',
             prefix: 'ExportPortals',
+            selectOptions: selectOptions,
+            formatOptions: formatOptions,
         }
 
         const dialog = window.dialog({
             title: 'Export',
             html: template(data),
         }).parent()
-
-        //$('.ui-dialog-buttonpane', dialog).remove()
 
         const windowHeight: number = $(window).height() ?? 100
         const windowWidth: number = $(window).width() ?? 100
