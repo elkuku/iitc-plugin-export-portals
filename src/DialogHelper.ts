6f716c0c22635dc8b44b500cb623cb024ef29367
elkuku <der.el.kuku@gmail.com>
2025-10-22 07:36:05 -0500

    Use object instead of Maps in template

--- src/DialogHelper.ts
+++ src/DialogHelper.ts
@@ -1,30 +1,20 @@
-// @ts-expect-error "wwwwwwwwww"
-import html from './template/dialog.hbs' with {type: 'text'}
+// @ts-expect-error "Import attributes are only supported when the --module option is set to esnext, nodenext, or preserve"
+import dialogTemplate from './templates/dialog.hbs' with {type: 'text'}
 
 import * as Handlebars from 'handlebars'
 
-Handlebars.registerHelper('eachInMap', (map: Map<string, string>, block) => {
-    let out = ''
-    for (const [key, value] of map) {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-        const str: string = block.fn({ key, value })
-        out += str
-    }
-    return out
-})
-
 export class DialogHelper {
     public getDialog(): JQuery {
-        const template: HandlebarsTemplateDelegate = Handlebars.compile(html)
+        const template: HandlebarsTemplateDelegate = Handlebars.compile(dialogTemplate)
 
-        const selectOptions = new Map([
-            ['view', 'View'],
-            ['polygon', 'Polygon(s)'],
-        ])
+        const selectOptions = {
+            view: 'View',
+            polygon: 'Polygon(s)',
+        }
 
-        const formatOptions = new Map([
-            ['json', 'JSON'],
-        ])
+        const formatOptions = {
+            json: 'JSON'
+        }
 
         const data = {
             plugin: 'window.plugin.ExportPortals',
@@ -40,10 +30,12 @@ export class DialogHelper {
 
         const windowHeight: number = $(window).height() ?? 100
         const windowWidth: number = $(window).width() ?? 100
+
         const dialogHeight: number = dialog.height() ?? 50
         const dialogWidth: number = dialog.width() ?? 50
 
-        dialog.css('width', '600px')
+        dialog
+            .css('width', '600px')
             .css('top', (windowHeight - dialogHeight) / 2)
             .css('left', (windowWidth - dialogWidth) / 2)
 
